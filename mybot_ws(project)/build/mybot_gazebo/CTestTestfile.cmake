45045ea51793fae967f199120afcdce685cd5d95
ronnie7z7z <ronitrshukla37@gmai.com>
2020-08-06 23:04:34 +0530

    Bot - Has all abilities upto 2nd tutorial of Richard Wang

---
+++ mybot_ws(project)/build/mybot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ronnie7z/mybot_ws/src/mybot_gazebo
+# Build directory: /home/ronnie7z/mybot_ws/build/mybot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
