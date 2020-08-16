57e542ae4639c4915aaaf48e5cc02bd0a521ba8a
Nivedya-27 <190070039@iitb.ac.in>
2020-08-16 21:30:52 +0530

    frontier modifications

--- path_planning/mybot_navigation/scripts/recognizer_node.py
+++ path_planning/mybot_navigation/scripts/recognizer_node.py
@@ -1,35 +1,25 @@
 #! /usr/bin/env python
 import rospy
 from std_msgs.msg import Float32MultiArray
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from mybot_navigation.msg import status_info
-#from geometry_msgs.msg import Twist
+import os
+import time
 
 #id for the exit photo when training is 3,say
 #id for a template of frame is 4, say
 
 class Recognizer():
 	def __init__(self):
-		self.state=status_info()
-		rospy.init_node('explorer',anonymous=True)
-		self.sub2=rospy.Subscriber("/amcl_pose",PoseWithCovarianceStamped,self.cb)
+		rospy.init_node('recognizer',anonymous=True)
 		self.sub=rospy.Subscriber("/objects", 1, self.callback)
-		self.pub=rospy.Publisher("/status",status_info)
-
-	def cb(self,data):
-		self.state.now=data.pose.pose.position
 
 	def callback(self,msg):
 		id=msg.data[0]
 		if id==3:
-			#exit point discovered
-			self.state.type='exit'
-		elif id==4:
-			self.state.type='image'
+			time.sleep(10.0)
+			os.system('rosnode kill /video_record_camera1 /slam_gmapping /amcl')
+			os.system('rosnode kill /explore /move_base /gmapping_node /find_object_2d')
 		else:
-			self.state.type='ignored'
-		rospy.loginfo(self.state.type)
-		self.pub.publish(self.state)
+			print('exit not reached yet')
 
 if __name__=='__main__':
 	try:
